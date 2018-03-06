7023d85bdd73752e08231741bce935e59c3de82e
Konstantin Lynda <knlynda@gmail.com>
2018-03-06 22:51:09 +0100

    add device secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -58,6 +58,7 @@ end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = APP_CONFIG.devise_secret_key
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
